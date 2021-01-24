0e7386bfca17fbe7940d0ebed5bee0a61423038c
Blake A. Long <blake.a.long@gmail.com>
2021-01-24 15:33:04 -0700

    imported .jwt and .jwt.resource packages and all files for JWT setup from past project

---
+++ src/main/java/com/blakelong/reactspringtodo/jwt/JwtUserDetailsService.java
@@ -0,0 +1,43 @@
+package com.blakelong.reactspringtodo.jwt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.blakelong.reactspringtodov2.dao.UserRepository;
+import com.blakelong.reactspringtodov2.entity.User;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+	private UserRepository userRepository;
+	
+	@Autowired
+	public JwtUserDetailsService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username);
+		
+		if (user == null) {
+			throw new UsernameNotFoundException(String.format("USER_NOT_FOUND '%s'.", username));
+		} else {
+			return create(user);
+		}
+	}
+	
+	public static JwtUserDetails create(User user) {
+		List<SimpleGrantedAuthority> roles = new ArrayList<SimpleGrantedAuthority>();
+		roles.add(new SimpleGrantedAuthority(user.getRole()));
+		return new JwtUserDetails(user.getId(), user.getUsername(), user.getPassword(), roles);
+	}
+	
+}
---
+++ src/main/java/com/blakelong/reactspringtodo/jwt/resource/AuthenticationException.java
@@ -0,0 +1,7 @@
+package com.blakelong.reactspringtodo.jwt.resource;
+public class AuthenticationException extends RuntimeException {
+    public AuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
