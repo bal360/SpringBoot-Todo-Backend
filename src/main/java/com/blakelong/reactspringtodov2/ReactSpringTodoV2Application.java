96c1ccbd7a3b38f11b12ada6fd72667d5f67328d
Blake A. Long <blake.a.long@gmail.com>
2021-01-24 12:24:07 -0700

    second commit

---
+++ src/main/java/com/blakelong/reactspringtodov2/ReactSpringTodoV2Application.java
@@ -0,0 +1,13 @@
+package com.blakelong.reactspringtodov2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringTodoV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringTodoV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/blakelong/reactspringtodov2/ReactSpringTodoV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blakelong.reactspringtodov2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringTodoV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
