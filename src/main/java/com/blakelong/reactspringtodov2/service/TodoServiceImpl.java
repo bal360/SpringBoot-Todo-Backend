e65ccd762002e06ec092a9793aba956cc32109aa
Blake A. Long <blake.a.long@gmail.com>
2021-01-24 17:33:35 -0700

    @Service annotation to TodoServiceImpl

--- src/main/java/com/blakelong/reactspringtodov2/service/TodoServiceImpl.java
+++ src/main/java/com/blakelong/reactspringtodov2/service/TodoServiceImpl.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.blakelong.reactspringtodov2.dao.TodoRepository;
 import com.blakelong.reactspringtodov2.entity.Todo;
 
+@Service
 public class TodoServiceImpl implements TodoService {
 
 	private TodoRepository todoRepository;
